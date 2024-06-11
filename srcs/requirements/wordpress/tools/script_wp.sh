48cc811783c19888720070ed1804d5aced810803
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-06-11 18:03:13 +0200

    changing wordpress files again

--- srcs/requirements/wordpress/tools/script_wp.sh
+++ srcs/requirements/wordpress/tools/script_wp.sh
@@ -1,115 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# Environment variables
-WORDPRESS_DB_NAME=wordpress
-WORDPRESS_DB_USER=jsarabia
-WORDPRESS_USER_PASSWORD=42
-WORDPRESS_DB_HOST=mariadb
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
 
-# Path to the wp-config.php file
-WP_CONFIG_PATH="/var/www/html/wp-config.php"
+####### MANDATORY PART ##########
 
-# Create the wp-config.php file
-cat <<EOF > $WP_CONFIG_PATH
-<?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', '$WORDPRESS_DB_NAME' );
-
-/** MySQL database username */
-define( 'DB_USER', '$WORDPRESS_DB_USER' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', '$WORDPRESS_USER_PASSWORD' );
-
-/** MySQL hostname */
-define( 'DB_HOST', '$WORDPRESS_DB_HOST' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-\$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
-}
-
-/** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
-EOF
-
-# Ensure the correct permissions
-chown www-data:www-data $WP_CONFIG_PATH
-chmod 640 $WP_CONFIG_PATH
-
-echo "wp-config.php has been created and configured."
-
+	#Inport env variables in the config file
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+    
 /usr/sbin/php-fpm7.4 --nodaemonize
