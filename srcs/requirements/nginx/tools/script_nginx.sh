383b5de2a88b87ecd12878502f297481ee30ed43
Javier Sarabia <jsarabia98@gmail.com>
2024-06-18 16:22:54 +0200

    creating nginx certificates in a new way

--- srcs/requirements/nginx/tools/script_nginx.sh
+++ srcs/requirements/nginx/tools/script_nginx.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
 # Create SSL certificates
-openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=$DOMAIN_NAME/C=EU/L=Madrid" -keyout /etc/ssl/rootjsarabia.key -out etc/ssl/rootjsarabia.crt
-envsubst '$DOMAIN_NAME'< /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/nginx.conf
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/jsarabia.42.fr.key \
+    -out /etc/ssl/certs/jsarabia.42.fr.crt \
+    -subj "/C=XX/ST=XX/L=XX/O=XX/OU=XX/CN=jsarabia.42.fr"
 nginx -g 'daemon off;'
