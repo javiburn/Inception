f7c51321d6a5ad2941eb3879e3d72c16c0b4b071
Javier Sarabia <jsarabia98@gmail.com>
2024-06-18 16:31:50 +0200

    ssl certificates created in dockerfile

--- srcs/requirements/nginx/tools/script_nginx.sh
+++ srcs/requirements/nginx/tools/script_nginx.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
 
-# Create SSL certificates
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/ssl/private/jsarabia.42.fr.key \
-    -out /etc/ssl/certs/jsarabia.42.fr.crt \
-    -subj "/C=XX/ST=XX/L=XX/O=XX/OU=XX/CN=jsarabia.42.fr"
 nginx -g 'daemon off;'
