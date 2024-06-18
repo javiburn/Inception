a1ebdb905bfa875fff95eb3e2ba0c63738a9ab43
Javier Sarabia <jsarabia98@gmail.com>
2024-06-18 16:06:23 +0200

    changing initillization scripts

--- srcs/requirements/mariadb/tools/script_db.sh
+++ srcs/requirements/mariadb/tools/script_db.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-# Start the service MySQL:
-service mysql start
+MYSQL_INIT_FILE="/createdb.sql"
 
-# Create the table if it does not exist already:
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${WORDPRESS_DB_NAME}\`;"
+chown -R mysql: /var/lib/mysql
+chmod 777 /var/lib/mysql
 
-# Create a user that can manipulate the table if it does not exist already:
-mysql -e "CREATE USER IF NOT EXISTS \`${WORDPRESS_DB_USER}\`@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
+mysql_install_db >/dev/null 2>&1
 
-# Grants all rights to the new user created to manipulate the database
-mysql -e "GRANT ALL PRIVILEGES ON \`${WORDPRESS_DB_NAME}\`.* TO \`${WORDPRESS_DB_USER}\`@'%' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
-
-# Change the password of the root:
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${WORDPRESS_USER_PASSWORD}';"
-
-# Flushes so that it refreshes and MYSQL takes the changes into account:
-mysql -e "FLUSH PRIVILEGES;"
-
-# Restarts MYSQL for the changes to take effect:
-mysqladmin -u root -p$WORDPRESS_USER_PASSWORD shutdown
-exec mysqld_safe
+if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    rm -f "$MYSQL_INIT_FILE"
+    echo "CREATING DATABASE, USERS and PASSWORDS..."
+    echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
+    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
+    echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+    echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+    echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
+    echo "Starting database..."
+    mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
+else
+    echo "Starting database..."
+    mysqld_safe >/dev/null 2>&1
+fi
