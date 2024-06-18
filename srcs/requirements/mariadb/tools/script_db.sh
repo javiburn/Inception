499b6a77477f36865fee3b790bc128afc4fecedb
Javier Sarabia <jsarabia98@gmail.com>
2024-06-18 17:34:38 +0200

    creating users in database

--- srcs/requirements/mariadb/tools/script_db.sh
+++ srcs/requirements/mariadb/tools/script_db.sh
@@ -7,18 +7,12 @@ chmod 777 /var/lib/mysql
 
 mysql_install_db >/dev/null 2>&1
 
-if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-    rm -f "$MYSQL_INIT_FILE"
-    echo "CREATING DATABASE, USERS and PASSWORDS..."
-    echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
-    echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
-    echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
-    echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
-    echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
-    echo "Starting database..."
-    mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
-else
-    echo "Starting database..."
-    mysqld_safe >/dev/null 2>&1
-fi
+echo "CREATING DATABASE, USERS and PASSWORDS..."
+echo "CREATE DATABASE $MYSQL_DATABASE;" >> "$MYSQL_INIT_FILE"
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> "$MYSQL_INIT_FILE"
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> "$MYSQL_INIT_FILE"
+echo "FLUSH PRIVILEGES;" >> "$MYSQL_INIT_FILE"
+echo "Starting database..."
+mysqld_safe --init-file=$MYSQL_INIT_FILE >/dev/null 2>&1
