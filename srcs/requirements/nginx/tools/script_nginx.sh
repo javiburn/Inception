e6b3dd5242a74d766d0ffe5c71b128e6bf047ba1
Javier Sarabia <jsarabia98@gmail.com>
2024-06-18 16:38:17 +0200

    changing certs

--- srcs/requirements/nginx/tools/script_nginx.sh
+++ srcs/requirements/nginx/tools/script_nginx.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/jsarabia.key \
+    -out /etc/ssl/certs/jsarabia.crt \
+    -subj "/C=XX/ST=XX/L=XX/O=XX/OU=XX/CN=jsarabia.42.fr"
+
 nginx -g 'daemon off;'
