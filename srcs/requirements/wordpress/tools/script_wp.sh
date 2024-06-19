02ff92b03df7fa04e904fed28986f46799f102b1
Javier Sarabia <jsarabia98@gmail.com>
2024-06-19 15:13:13 +0200

    fixing errors when creating wp-config

--- srcs/requirements/wordpress/tools/script_wp.sh
+++ srcs/requirements/wordpress/tools/script_wp.sh
@@ -9,12 +9,15 @@ done
 
 cd /var/www/html
 
-if [ -f /var/www/html/wp-config.php ]; then
+# Download WordPress core files if not already present
+if [ ! -f /var/www/html/wp-config.php ]; then
+  wp core download --path=/var/www/html --allow-root
+fi
+
+if [ -f wp-config.php ]
 then
     echo "WordPress already installed."
 else
-    # Download WordPress core files if not already present
-    wp core download --path=/var/www/html --allow-root
     echo "Creating wp-config.php..."
     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
     if [ $? -ne 0 ]; then
